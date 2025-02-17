a1770ef1824999a43a1f62f315c77f4135d98897
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-18 01:11:57 +0300

    Update duplifile.cs

--- code/duplifile.cs
+++ code/duplifile.cs
@@ -43,3 +43,82 @@ public class GelismisHesaplayici
         return a / b;
     }
 }
+using System;
+
+public class DuplicateCodeExample1
+{
+    public void PrintSquare(int number)
+    {
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+    }
+
+    public void PrintCube(int number)
+    {
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+
+    public void PrintSquareAndCube(int number)
+    {
+        // Tekrar eden kod: kare hesaplama
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+
+        // Tekrar eden kod: küp hesaplama
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+}
+
+public class DuplicateCodeExample2
+{
+    public void PrintSquare(int number)
+    {
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+    }
+
+    public void PrintCube(int number)
+    {
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+
+    public void PrintSquareAndCube(int number)
+    {
+        // Tekrar eden kod: kare hesaplama
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+
+        // Tekrar eden kod: küp hesaplama
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+}
+
+public class DuplicateCodeExample3
+{
+    public void PrintSquare(int number)
+    {
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+    }
+
+    public void PrintCube(int number)
+    {
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+
+    public void PrintSquareAndCube(int number)
+    {
+        // Tekrar eden kod: kare hesaplama
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+
+        // Tekrar eden kod: küp hesaplama
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+}
