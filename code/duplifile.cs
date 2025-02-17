cb6c108a0b000e3fb188388d4754074a1202d7ec
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-18 00:49:46 +0300

    Update and rename duplifile to duplifile.cs

---
+++ code/duplifile.cs
@@ -0,0 +1,73 @@
+public class Hesaplayici
+{
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Cikar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Carp(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Bol(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+public class GelismisHesaplayici
+{
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Cikar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Carp(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Bol(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+using System;
+
+public class DuplicateCodeExample
+{
+    public void PrintSquare(int number)
+    {
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+    }
+
+    public void PrintCube(int number)
+    {
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+
+    public void PrintSquareAndCube(int number)
+    {
+        // Duplicate code for calculating square
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+
+        // Duplicate code for calculating cube
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+}
