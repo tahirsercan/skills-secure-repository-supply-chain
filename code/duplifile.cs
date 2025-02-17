af98957c4d01782af4ee0030b10bb2ecd0ce0cd2
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-18 01:07:01 +0300

    Update duplifile.cs

--- code/duplifile.cs
+++ code/duplifile.cs
@@ -98,3 +98,54 @@ public class DuplicateCodeExample
         Console.WriteLine("The cube of " + number + " is " + cube);
     }
 }
+public class DuplicateCodeExample
+{
+    public void PrintSquare(int number)
+    {
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+    }
+
+    public void PrintCube(int number)
+    {
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+
+    public void PrintSquareAndCube(int number)
+    {
+        // Duplicate code for calculating square
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+
+        // Duplicate code for calculating cube
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+}
+public class DuplicateCodeExample
+{
+    public void PrintSquare(int number)
+    {
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+    }
+
+    public void PrintCube(int number)
+    {
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+
+    public void PrintSquareAndCube(int number)
+    {
+        // Duplicate code for calculating square
+        int square = number * number;
+        Console.WriteLine("The square of " + number + " is " + square);
+
+        // Duplicate code for calculating cube
+        int cube = number * number * number;
+        Console.WriteLine("The cube of " + number + " is " + cube);
+    }
+}
+
