a317e6eb3cf7e55ae8316f9d773729c3a8a66c93
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-19 01:16:47 +0300

    Update duplifile.cs

--- code/duplifile.cs
+++ code/duplifile.cs
@@ -43,3 +43,93 @@ public class GelismisHesaplayici
         return a / b;
     }
 }
+public class Hesaplayici
+{
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Cikar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Carp(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Bol(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+public class GelismisHesaplayici
+{
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Cikar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Carp(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Bol(int a, int b)
+    {
+        return a / b;
+    }
+}
+public class Hesaplayici
+{
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Cikar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Carp(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Bol(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+public class GelismisHesaplayici
+{
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Cikar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Carp(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Bol(int a, int b)
+    {
+        return a / b;
+    }
+}
